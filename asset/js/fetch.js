98ca926ff2df45a57d64865aaabb9c5c3834387e
swokamoto <swokamoto@gmail.com>
2024-06-17 12:54:44 -0400

    debugg, HTT'P'S' corrected

--- asset/js/fetch.js
+++ asset/js/fetch.js
@@ -2,7 +2,7 @@ const API_KEY = "950568ca8ba77b218222e73059c2fc24";
 
 // get the lat and lon of a city using the Geocoder API
 const getLatLon = (cityName) => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`)    
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`)    
     .then(response => response.json())
     .then(data => {
         getWeatherData(data.city.coord.lat, data.city.coord.lon);
@@ -16,7 +16,7 @@ const getLatLon = (cityName) => {
 const getWeatherData = (lat, lon) => {
     lat = lat.toFixed(2);
     lon = lon.toFixed(2);
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
         .then(response => response.json())
         .then(data => {
             generateWeather(data); // fill out the weather card and the 5 day forecast
