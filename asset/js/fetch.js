556be4609642f0a776ae400ed10dd48d5b7db944
swokamoto <swokamoto@gmail.com>
2024-06-17 11:46:15 -0400

    added history

--- asset/js/fetch.js
+++ asset/js/fetch.js
@@ -1,13 +1,18 @@
+// When the search button is clicked, the functions run in a specific order. 
+// 1st getLatLon(cityName), fetches the latitude and longitude of the city. 
+// 2nd getWeatherData(lat,lon), fetches the weather data. 
+// 3rd generateWeather(data), populates the weather card and the 5-day forecast.
+
 const API_KEY = "950568ca8ba77b218222e73059c2fc24";
 
-// get the lat and lon of a city
+// get the lat and lon of a city using the Geocoder API
 const getLatLon = (cityName) => {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`)    
     .then(response => response.json())
-        .then(data => {
-            getWeatherData(data.city.coord.lat, data.city.coord.lon);
-        })
-        .catch(error => console.error(error));
+    .then(data => {
+        getWeatherData(data.city.coord.lat, data.city.coord.lon);
+    })
+    .catch(alert("City not found"));
 } 
 
 // get weather data from openweathermap API using lat and lon
@@ -24,7 +29,8 @@ const getWeatherData = (lat, lon) => {
 
 // fill out the weather card and the 5 day forecast
 const generateWeather = (data) => {
-    $("#main-title").text(data.city.name);
+    // fill out the main-title with name and date
+    $("#main-title").text(data.city.name + " " + new Date(data.list[0].dt_txt).toLocaleDateString());
     $("#main-icon").attr("src", `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`);
     let temp = Math.round((data.list[0].main.temp - 273.15) * 9/5 + 32);
     $("#main-temp").text("Temp: " + temp + "°F");
@@ -41,5 +47,25 @@ const generateWeather = (data) => {
         $(`#${i}`).find(".wind").text("Wind: " + data.list[i].wind.speed + "MPH");
         $(`#${i}`).find(".humidity").text("Humidity: " + data.list[i].main.humidity + "%");
     }   
+
+    //if the city is not in the history, add it to the history
+    let history = JSON.parse(localStorage.getItem("history")) || [];
+    let city = history.find(city => city.name === data.city.name);
+    if (city === undefined) {
+        let cityStore = {
+            name: data.city.name,
+            data: data
+        };
+        history.unshift(cityStore);
+        localStorage.setItem("history", JSON.stringify(history));
+        addHistory(data);
+    }
+
 }
 
+// add a button to the history div
+const addHistory = (data) => {
+        let button = $('<button type="submit" class="btn btn-info">').text(data.city.name);
+        button.data("info", data);
+        $("#history").prepend(button);
+}
